97a404819e90db874a66c8e8c64a5b45ae9134f1
jacelia <jacelia.costa@gmail.com>
2024-08-28 15:48:49 -0300

    primeiro commit da soma de digitos

---
+++ README.md
@@ -0,0 +1,1 @@
+# soma dos dígitos
---
+++ somadosdigitos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace somadosdigitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+            int digito;
+            int soma = 0;
+
+            Console.Write("Digite um número: ");
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            while (numero > 0) {
+                digito = numero % 10;
+                soma = soma + digito;
+                numero = numero / 10;
+            }
+            Console.WriteLine("Soma = " + soma);
+            Console.ReadKey();
+        }
+    }
+}
